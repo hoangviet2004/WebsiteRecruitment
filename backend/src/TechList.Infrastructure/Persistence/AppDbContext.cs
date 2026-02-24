8b9ddda918c624e3a28b4b863645310dfd44c8bd
Bongmaanhquoc-exe <longdontcry@gmail.com>
2026-02-24 12:35:46 +0700

    tạo các forder theo cấu trúc clean architecture, set up, dowload những package cần thiết

---
+++ backend/src/TechList.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TechList.Infrastructure.Identity; // ✅ Sửa namespace
+
+namespace TechList.Infrastructure.Persistence;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<ApplicationUser>().ToTable("Users");
+    }
+}
---
+++ backend/tests/TechList.API.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TechList.API.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
