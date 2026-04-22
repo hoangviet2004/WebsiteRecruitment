16bb1d29a752c3f25c8418cd46d8afda23d45da3
hoangviet2004 <quachhoangviet2004@gmail.com>
2026-04-22 09:30:12 +0700

    sửa lỗi database quản lý công ty

---
+++ backend/src/TechList.Infrastructure/Persistence/Migrations/20260422022613_AddCompanySize.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechList.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanySize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
