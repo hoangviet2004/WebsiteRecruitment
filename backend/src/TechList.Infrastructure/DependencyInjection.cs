1bc20395c32a8455d92b0b94de9734b0b4246506
hoangviet2004 <quachhoangviet2004@gmail.com>
2026-05-17 17:04:27 +0700

    sửa lỗi retry migrate DB

--- backend/src/TechList.Infrastructure/DependencyInjection.cs
+++ backend/src/TechList.Infrastructure/DependencyInjection.cs
@@ -43,7 +43,8 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
     {
         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(config.GetConnectionString("DefaultConnection"))
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection"), sql =>
+                sql.EnableRetryOnFailure(maxRetryCount: 8, maxRetryDelay: TimeSpan.FromSeconds(15), errorNumbersToAdd: null))
                    .ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning)));
 
         services.AddIdentityCore<ApplicationUser>(options =>
